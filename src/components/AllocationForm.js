3f303a2f07aa1ec310f67cd1b281bfd669543178
Evan Washington <enavy04@gmail.com>
2024-04-24 01:52:05 -0700

    Connected to currency in AppContext  Minor formatting

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const AllocationForm = (props) => {
-    const { dispatch,remaining  } = useContext(AppContext);
+    const { dispatch, remaining, currency } = useContext(AppContext);
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
@@ -58,16 +58,16 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                  {/* create label for input with id cost.  Label should be touching the left side of the input. */}
+                    <label htmlFor='cost' style={{ marginLeft: '2rem', fontSize:'1.5em'}}>{currency}</label>
                     <input
                         required='required'
                         type='number'
                         id='cost'
                         value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
+                        style={{ marginLeft: '.25rem', size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
-
                     <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
                         Save
                     </button>
