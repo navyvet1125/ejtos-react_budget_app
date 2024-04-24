842454621681625a8652f5d8c5b78b8b145f0959
Evan Washington <enavy04@gmail.com>
2024-04-23 21:43:34 -0700

    Added budget limit

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -5,11 +5,15 @@ const Budget = () => {
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
+        if(event.target.value > 20000) {
+            alert("Budget cannot exceed £20000");
+            return;
+        }
         setNewBudget(event.target.value);
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
+<span>Budget: £</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
