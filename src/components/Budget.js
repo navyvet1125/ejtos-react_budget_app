4eadf5ceed4be4cbb2ad1258b0d925d5c8cfdab3
Evan Washington <enavy04@gmail.com>
2024-04-24 01:53:39 -0700

    Wired to add currency change functionality.  Fixed budgeting logic

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,18 +2,25 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, expenses, currency } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
     const handleBudgetChange = (event) => {
         if(event.target.value > 20000) {
             alert("Budget cannot exceed £20000");
+            return;
+        }
+        if(event.target.value < totalExpenses) {
+            alert("Budget cannot be less than total expenses");
             return;
         }
         setNewBudget(event.target.value);
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £</span>
+<span>Budget: {currency}</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
