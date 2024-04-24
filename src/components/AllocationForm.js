670fa58cab673cc0b18590e23ae22eece61ca32d
Evan Washington <enavy04@gmail.com>
2024-04-23 21:04:59 -0700

    Fixed bug that prevents reducing budget when cost is greater than remaining

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -10,7 +10,7 @@ const AllocationForm = (props) => {
 
     const submitEvent = () => {
 
-            if(cost > remaining) {
+            if((cost > remaining) && (action === "Add")) {
                 alert("The value cannot exceed remaining funds  £"+remaining);
                 setCost("");
                 return;
